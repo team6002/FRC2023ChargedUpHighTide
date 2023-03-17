ff78e72cdac251efb1039a2cc7c51e85fe6a711c
JUSTIN <justinnguyen4675@gmail.com>
2023-03-16 23:20:56 -0400

    added ground commands

--- src/main/java/frc/robot/commands/CMD_GroundConeDownIntake.java
+++ src/main/java/frc/robot/commands/CMD_GroundConeDownIntake.java
@@ -18,13 +18,13 @@ import frc.robot.subsystems.SUB_FiniteStateMachine.RobotState;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class CMD_GroundConeDownIntake extends SequentialCommandGroup {
   /** Creates a new GroundIntake. */
-  public CMD_GroundConeUprightIntake(SUB_Intake p_intake, SUB_Elbow p_elbow, SUB_Elevator p_elevator, SUB_FiniteStateMachine p_finiteStateMachine) {
+  public CMD_GroundConeDownIntake(SUB_Intake p_intake, SUB_Elbow p_elbow, SUB_Elevator p_elevator, SUB_FiniteStateMachine p_finiteStateMachine) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new CMD_setState(p_finiteStateMachine, RobotState.INTAKE),
       new CMD_ElevatorSetPosition(p_elevator, ElevatorConstants.kElevatorGround),
-      new CMD_ElbowSetPosition(p_elbow, ElbowConstants.kElbowGroundConeUpright)
+      new CMD_ElbowSetPosition(p_elbow, ElbowConstants.kElbowGroundConeDown)
       
     );
   }
--- src/main/java/frc/robot/commands/CMD_GroundCubeIntake.java
+++ src/main/java/frc/robot/commands/CMD_GroundCubeIntake.java
@@ -5,15 +5,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.ElbowConstants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.subsystems.SUB_Elbow;
+import frc.robot.subsystems.SUB_Elevator;
+import frc.robot.subsystems.SUB_FiniteStateMachine;
+import frc.robot.subsystems.SUB_Intake;
+import frc.robot.subsystems.SUB_FiniteStateMachine.RobotState;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class GroundIntake extends SequentialCommandGroup {
+public class CMD_GroundCubeIntake extends SequentialCommandGroup {
   /** Creates a new GroundIntake. */
-  public GroundIntake() {
+  public CMD_GroundCubeIntake(SUB_Intake p_intake, SUB_Elbow p_elbow, SUB_Elevator p_elevator, SUB_FiniteStateMachine p_finiteStateMachine) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      new CMD_setState(p_finiteStateMachine, RobotState.INTAKE),
+      new CMD_ElevatorSetPosition(p_elevator, ElevatorConstants.kElevatorGround),
+      new CMD_ElbowSetPosition(p_elbow, ElbowConstants.kElbowGroundCube)
+      
+    );
   }
 }
