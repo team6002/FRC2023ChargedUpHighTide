ae25ad4d602572b1161578d480e1c468ad3416a3
Chris <cbnguyen3@gmail.com>
2023-04-13 00:22:08 -0400

    switch to using crcb channels and begin finding contours

--- src/main/java/frc/robot/subsystems/SUB_IntakeCamera.java
+++ src/main/java/frc/robot/subsystems/SUB_IntakeCamera.java
@@ -42,7 +42,7 @@ public class SUB_IntakeCamera extends SubsystemBase {
     /* THREAD FOR RUNNING CAMERASERVER AND OPENCV PROCESSING */
     class OpenCVThread implements Runnable {
         private int camWidth = 320, camHeight = 240;
-        private int CB_CHAN_IDX = 0;
+        private int CB_CHAN_IDX = 1;
 
         public void run() {
             UsbCamera camera = CameraServer.startAutomaticCapture();
@@ -52,42 +52,50 @@ public class SUB_IntakeCamera extends SubsystemBase {
             CvSource outputStream = CameraServer.putVideo("Rectangle", camWidth, camHeight);
 
             Mat mat = new Mat();
+            Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(6, 6));
+            Mat org;
 
             System.out.println("AAA: SETUP CAMERA COMPLETE");
 
             while (!Thread.interrupted()) {
-                if (cvSink.grabFrame(mat) == 0) {
-                    /* Error grabbing frame, ignore */
-                    System.out.println("AAA: ERROR GRABBING FRAMES");
-                    continue;
-                }
+                try {
+                    if (cvSink.grabFrame(mat) == 0) {
+                        /* Error grabbing frame, ignore */
+                        System.out.println("AAA: ERROR GRABBING FRAMES");
+                        continue;
+                    }
 
-                Imgproc.GaussianBlur(mat, mat, new Size(11, 11), 0);
-                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2YCrCb);
-                // Core.extractChannel(mat, mat, CB_CHAN_IDX);
-                // extract channel
-                // threshold
-                // morph mask
-
-                // Core.inRange(mat, new Scalar(100, 0, 0), new Scalar(130,255,255), mat);
-
-                // List<MatOfPoint> contours = new ArrayList<>();
-
-                // Imgproc.findContours(mat, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-                // Scalar color = new Scalar(0, 255, 0);
-
-                // for (int i = 0; i < contours.size(); i++) {
-                //     RotatedRect r = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
-                //     Size s = r.size;
-                //     double ratio = s.height / s.width;
+                    org = mat.clone();
     
-                //     if (ratio >= 0.6 && ratio <= 1.4) {
-                //         Imgproc.drawContours(org, contours, i, color, 1);
-                //     }
-                // }
+                    // Imgproc.GaussianBlur(mat, mat, new Size(11, 11), 0);
+                    Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2YCrCb);
+                    Core.extractChannel(mat, mat, CB_CHAN_IDX);
+                    Imgproc.threshold(mat, mat, 175, 255, Imgproc.THRESH_BINARY);
 
-                outputStream.putFrame(mat);
+                    Imgproc.dilate(mat, mat, dilateElement);
+                    Imgproc.dilate(mat, mat, dilateElement);
+                    Imgproc.dilate(mat, mat, dilateElement);
+                    Imgproc.dilate(mat, mat, dilateElement);
+                    Imgproc.dilate(mat, mat, dilateElement);
+                    Imgproc.dilate(mat, mat, dilateElement);
+    
+                    List<MatOfPoint> contours = new ArrayList<>();
+                    Imgproc.findContours(mat, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+    
+                    Scalar color = new Scalar(0, 255, 0);
+    
+                    for (int i = 0; i < contours.size(); i++) {
+                        Imgproc.drawContours(org, contours, i, color, 1);
+                        // RotatedRect r = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+                    }
+    
+                    outputStream.putFrame(org);
+
+                    /* Sleep for 10ms to avoid overruning the RoboRio */
+                    Thread.sleep(10);
+                } catch (Exception e) {
+                    System.out.println("OpenCVThread Exception: " + e);
+                }
             }
         }
     }
